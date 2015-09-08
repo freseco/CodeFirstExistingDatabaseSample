1ad26555108c69e3ba12309e799f95ccf4e401b1
freseco <freseco@gmail.com>
2015-09-08 14:20:33 -0700

    prueba1

---
+++ CodeFirstExistingDatabaseSample/BloggingContext.cs
@@ -0,0 +1,24 @@
+namespace CodeFirstExistingDatabaseSample
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext()
+            : base("name=BloggingContext")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ CodeFirstExistingDatabaseSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExistingDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new BloggingContext())
+            {
+
+                Console.WriteLine("Enter a name for a new blog");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                //creamos la clase, add class to the database
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in database:");
+                foreach(var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
